583886e2b37ca3479eca96d36ec8bfdebcc81297
ZCYBORG-404 <51997622+ZCYBORG-404@users.noreply.github.com>
2019-06-22 10:04:30 +0800

    Add files via upload

---
+++ salamZCYBORG.sh
@@ -0,0 +1,18 @@
+clear
+echo "hallo guys saya ZCYBORG saya seorang hacker HAHAHA" | lolcat
+echo ""
+sleep 2
+echo "hobi saya adalah membuat script hacking" | lolcat
+sleep 5 
+clear
+
+echo "========================================" | lolcat
+echo "instagram:@everyday_meme.id.in" | lolcat
+echo "facebook :redy todek" | lolcat
+echo "github   :https://github.com/ZCYBORG-404" | lolcat
+echo "note:jadilah hacker yang jahat saat kau di hina" | lolcat
+echo "========================================" | lolcat
+sleep 3
+echo "NANTIKAN TOOLS BUATAN SAYA BYEEE" | lolcat
+sleep 2
+exit
